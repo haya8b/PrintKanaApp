46968047eb611bacee7214373e880297b3e2a283
片岡勇人 <>
2019-06-05 00:33:28 +0900

    Project commit

---
+++ PrintKanaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrintKanaApp
+//
+//  Created by 片岡勇人 on 2019/06/05.
+//  Copyright © 2019 HayatoKataoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
